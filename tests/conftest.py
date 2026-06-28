220dc336a151bb0882f2cfdfb61df979a7f4bfcc
Abinav Katuru <akaturu@gmail.com>
2026-06-28 02:00:30 -0400

    Fix mypy config and conftest import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 
+import rad_peer_review_analytics.cli as cli
+from rad_peer_review_analytics.analytics import AnalyticsEngine
+
 
 @pytest.fixture(autouse=True)
 def _reset_cli_globals():
-    import rad_peer_review_analytics.cli as cli
-
     cli._reviews = []
     cli._reviewers = {}
-    cli._engine = cli.AnalyticsEngine()
+    cli._engine = AnalyticsEngine()
     yield
